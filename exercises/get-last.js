319f942ba3c991b0be87ed5b5855ed85f835341a
Sabine Robart <sabine.robart@protonmail.com>
2019-03-17 17:24:54 +0100

    Add Get exercises.

--- exercises/get-last.js
+++ exercises/get-last.js
@@ -8,7 +8,9 @@
  */
 
 // Your code :
-
+const getLast = (subject) => {
+  return subject[subject.length -1]
+}
 //* Begin of tests
 const assert = require('assert')
 
--- exercises/get-length.js
+++ exercises/get-length.js
@@ -8,7 +8,9 @@
  */
 
 // Your code :
-
+const getLength = (subject) => {
+  return subject.length;
+}
 //* Begin of tests
 const assert = require('assert')
 
